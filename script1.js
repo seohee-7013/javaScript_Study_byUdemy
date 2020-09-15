bee27d4b9cc1596a988fbd8a92e708b7d80f444e
seohee <seohee4024@gmail.com>
2020-09-15 18:51:21 +0900

    javaScript study starter dataType

---
+++ script1.js
@@ -0,0 +1,18 @@
+var firstName= 'Kim';
+console.log(firstName);
+
+var lastName='Seohee';
+var age=25;
+
+console.log(lastName+age);
+
+var fullAge=true;
+console.log(fullAge);
+
+var job;  /*undefined*/ 
+console.log(job);
+
+job='Teacher';
+console.log(job); 
+
+
